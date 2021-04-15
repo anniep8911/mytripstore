538c92ab439a15ef5c00c0d1784fe4d36e356302
Annie Park <pjatopazz@gmail.com>
2021-04-15 13:36:22 +0900

    coomit

---
+++ js/common.js
@@ -0,0 +1,10 @@
+/*햄버거사이드바*/
+function bars() {
+    document.getElementById('sideSlide').style.width = "50vw";
+    document.getElementById('bars').style.color = "transparent";
+}
+
+function barsC() {
+    document.getElementById('sideSlide').style.width = "0%";
+    document.getElementById('bars').style.color= "#fff";
+}
